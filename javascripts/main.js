b2b39e760fc6c5c19cfabbee8d355ef51f58341b
Villager <elvis4600@gmail.com>
2018-02-02 13:56:57 +0800

    update my page in 2018-2

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   $('.exp_drop').click(function(){
-  	$('.drop_info').hide()
-    $('#'+$(this).data('drop-no')).toggle();
+  	// if any other drop_info has shown, hide it 
+  	if($('#'+$(this).data('drop-no')).is(':visible')==0){
+  		$('.drop_info').hide();
+  	}
+  	$('#'+$(this).data('drop-no')).toggle();
   });
 });
