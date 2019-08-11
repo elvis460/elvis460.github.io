293270a615894dbf8431b7738a023d06351df700
Villager <elvis4600@gmail.com>
2019-08-11 23:20:56 +0800

    Update(Skilltree): update skill tree and some image url

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.exp_drop').click(function(){
   	// if any other drop_info has shown, hide it 
-  	if($('#'+$(this).data('drop-no')).is(':visible')==0){
+  	if($('#'+$(this).data('drop-no')).is(':visible') == 0){
   		$('.drop_info').hide();
   	}
   	$('#'+$(this).data('drop-no')).toggle();
