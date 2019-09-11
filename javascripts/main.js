ff2f15f5e6b125a248cea9f3320c69d025d5422d
Villager <elvis4600@gmail.com>
2019-09-12 00:42:33 +0800

    feat(Dropdown): when exp droped add social height

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-  $('.exp_drop').click(function(){
-  	// if any other drop_info has shown, hide it 
-  	if($('#'+$(this).data('drop-no')).is(':visible') == 0){
-  		$('.drop_info').hide();
-  	}
-  	$('#'+$(this).data('drop-no')).toggle();
+$(document).ready(function() {
+  $('.exp_drop').click(function() {
+    $('.social').removeClass('block-add-height');
+    // if any other drop_info has shown, hide it
+    if ($('#' + $(this).data('drop-no')).is(':visible') == 0) {
+      $('.drop_info').hide();
+    }
+    $('#' + $(this).data('drop-no')).toggle();
+
+    if ($('#' + $(this).data('drop-no')).is(':visible') == 1) {
+      $('.social').addClass('block-add-height');
+    }
   });
 });
